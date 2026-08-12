4367a142f2d9958b5e2fc3833496d912743d36bd
Jonatan <147938164+Luckybastd@users.noreply.github.com>
2026-08-12 23:52:45 +0700

    Create index.php

---
+++ api_router/index.php
@@ -0,0 +1,5 @@
+<?php
+// Meneruskan request dari Vercel ke sistem public Laravel
+require __DIR__ . '/../public/index.php';<?php
+// Meneruskan request dari Vercel ke sistem public Laravel
+require __DIR__ . '/../public/index.php';
